56d7d30731e1c7164bd8925f81c297cb421d5100
lirostin <lirostin@gmail.com>
2015-11-04 20:11:08 +0300

    1st commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+
+    public static String someFunction(int x, int n) {
+        // do something
+        return "123";
+    }
+}
